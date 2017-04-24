ddbee2687604b6aea8ef33066109ae59e028e5a5
Jakub Nowicki <j.nowicki@student.uw.edu.pl>
2017-04-24 14:20:22 +0200

    dodane zawijanie przed "z"

--- R/str_wrap_sieroty.R
+++ R/str_wrap_sieroty.R
@@ -9,5 +9,6 @@ str_wrap_sieroty <- function(string, width) {
     x <- str_wrap(string = string, width = width)
     x <- gsub(pattern = ' w\n',replacement = '\nw ',x = x)
     x <- gsub(pattern = ' i\n',replacement = '\ni ',x = x)
+    x <- gsub(pattern = ' z\n',replacement = '\nz ',x = x)
     return(x)
 }

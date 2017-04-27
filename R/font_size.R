7f586dd76f8266203adf2a2347c4275f39ce9509
Jakub Nowicki <j.nowicki@student.uw.edu.pl>
2017-04-27 14:08:50 +0200

    Font size function

---
+++ R/font_size.R
@@ -0,0 +1,11 @@
+#' Dostosowuje wielkość czcionki
+#'
+#' Dostosowuje wielkość czcionki do geom_text
+#'
+#' @param x wielkość czcionki
+#'
+#' @export
+#'
+font.size <- function(x) {
+    x/(14/5)
+}

72f2f695dbae7bba05d82f48cbefabc6efdc431e
Jakub Nowicki <j.nowicki@student.uw.edu.pl>
2017-05-31 14:41:53 +0200

    axis colour option

--- R/theme_labstat.R
+++ R/theme_labstat.R
@@ -2,18 +2,20 @@
 #'
 #' @description ggplot2 LASiE theme.
 #'
+#' @param colour kolor osi
+#'
 #' @export
 #'
 
-theme_labstat <- function() {
+theme_labstat <- function(colour = 'grey') {
     theme(
         text = element_text(family = "Calibri"),
         axis.title = element_text(size = 9),
         axis.text = element_text(size = 9),
-        axis.ticks = element_line(colour = 'grey'),
+        axis.ticks = element_line(colour = colour),
         panel.background = element_blank(),
         axis.line = element_line(size = 0.5,
-                                 colour = 'grey'),
+                                 colour = colour),
         legend.title = element_text(size = 10),
         legend.text = element_text(size = 10),
         legend.position = ('top'),

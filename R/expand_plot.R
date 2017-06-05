f1a50751e8e5e50bcda00ac61e2cc6e18c7f04de
Jakub Nowicki <j.nowicki@student.uw.edu.pl>
2017-06-05 11:14:09 +0200

    minor add

--- R/expand_plot.R
+++ R/expand_plot.R
@@ -2,18 +2,19 @@
 #'
 #' @param plot wykres do rozszerzenia
 #' @param axis os do rozszerzenia
+#' @param mnoznik mnożnik powiększenia wykresu
 #'
 #' @export
 #'
-expand_plot <- function(plot, axis = "y") {
+expand_plot <- function(plot, axis = "y", mnoznik = 1.2) {
     zmienna <- plot$data[as.character(plot$mapping[axis])]
     tmp.max <- max(zmienna,na.rm = T)
     if (tmp.max<1) {
         tmp.max <- tmp.max*100
-        limit <- ceiling(tmp.max*1.2)
+        limit <- ceiling(tmp.max*mnoznik)
         limit <- limit/100
     } else {
-        limit <- ceiling(tmp.max*1.2)
+        limit <- ceiling(tmp.max*mnoznik)
     }
     if (axis == 'y') {
         output <- plot + expand_limits(y = limit)
